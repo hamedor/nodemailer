e0f3dd5ed762db3525eb32e1afd1d653cf552bac
Dmitriy <dnpopov@outlook.com>
2024-01-17 15:47:43 +0500

    fix

--- api.js
+++ api.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 var corsOptions = {
-  origin: 'https://main--legendary-snickerdoodle-b42463.netlify.app',
+  origin: '*',
   optionsSuccessStatus: 200
 }
 
